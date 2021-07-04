c9882c33ea914d4a16d793d58a2dab5ccb8c6282
Mr.Chi <chiqingjun@gmail.com>
2021-07-04 21:03:52 +0800

    Add router for example declaration

---
+++ tutorial/api/example_declaration.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+
+from typing import Optional
+
+from fastapi import APIRouter, Body
+from pydantic import BaseModel, Field, HttpUrl
+
+router = APIRouter()
+
+
+class User(BaseModel):
+    name: str
+    desc: Optional[str] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "ZhangSan",
+                "desc": "Leading role in videoes of Luoxiang.",
+            }
+        }
+
+
+class Item(BaseModel):
+    name: str = Field(..., example="saying")
+    desc: Optional[str] = Field(
+        None, example="All work, no play, makes a jack dull boy."
+    )
+
+
+class Image(BaseModel):
+    name: str
+    url: Optional[HttpUrl] = None
+
+
+@router.post("/coco/", summary="Declare request example in Field and Model")
+def coco(user: User, item: Item):
+    return {"user": user, "item": item}
+
+
+@router.post("/lifetimes/", summary="Declare request example in Body")
+def lifetimes(
+    image: Image = Body(
+        ...,
+        example={"name": "Mona Lisa", "url": "https://en.wikipedia.org/wiki/Mona_Lisa"},
+    )
+):
+    return {"image": image}
+
+
+@router.post(
+    "/lordofring/",
+    summary="Declare multiple examples",
+    description=(
+        "examples is **NOT** working.\n\n"
+        "See: [Declare Request Example Data \- FastAPI]"
+        "(https://fastapi.tiangolo.com/tutorial/schema-extra-example/#technical-details)"
+    ),
+)
+def lord_of_ring(
+    image: Image = Body(
+        ...,
+        examples={
+            "Leonardo da Vinci": {
+                "name": "Mona Lisa",
+                "url": "https://en.wikipedia.org/wiki/Mona_Lisa",
+            },
+            "Van Gogh": {
+                "name": "Sunflowers",
+                "url": "https://en.wikipedia.org/wiki/Sunflowers_(Van_Gogh_series)",
+            },
+        },
+    )
+):
+    return {"image": image}
--- tutorial/app.py
+++ tutorial/app.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 
-from tutorial.api import path_params, query_params, request_body
+from tutorial.api import path_params, query_params, request_body, example_declaration
 
 app = FastAPI(
     title="FastAPI Tutorial",
@@ -21,3 +21,8 @@ async def index():
 app.include_router(path_params.router, prefix="/pathparams", tags=["Path Params"])
 app.include_router(query_params.router, prefix="/queryparams", tags=["Query Params"])
 app.include_router(request_body.router, prefix="/requestbody", tags=["Request Body"])
+app.include_router(
+    example_declaration.router,
+    prefix="/example",
+    tags=["Example Declaration"],
+)
