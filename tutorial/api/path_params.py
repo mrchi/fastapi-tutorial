0e8eb99aa68cf63254712963c9bbce63b5fc053e
Mr.Chi <chiqingjun@gmail.com>
2021-07-04 15:55:06 +0800

    Update router for path params

--- tutorial/api/path_params.py
+++ tutorial/api/path_params.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 
+import time
 from enum import Enum
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
+from fastapi.param_functions import Query
 
 router = APIRouter()
 
@@ -45,3 +47,22 @@ def read_file(file_path: str):
 @router.get("/users/{user_id}/items/{item_id}", summary="Multiple path parameters")
 def get_user_items(user_id: int, item_id: int):
     return {"user_id": user_id, "item_id": item_id}
+
+
+@router.get("/items/{item_id}", summary="Path parameter with metadata")
+def get_item_detail(
+    item_id: int = Path(..., title="Item ID", description="Item ID of the item to get")
+):
+    return {"item_id": item_id}
+
+
+@router.get(
+    "/interstellar/{ts_int}/{ts_float}",
+    summary="Path parameters with additional validation",
+    tags=["Validation"],
+)
+def get_interstellar_timestamp(
+    ts_int: int = Path(..., ge=0, le=1625385023),
+    ts_float: float = Path(..., gt=100, lt=200),
+):
+    return {"ts_int": ts_int, "ts_float": ts_float}
