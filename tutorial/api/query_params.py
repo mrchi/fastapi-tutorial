a346d6a2933b3a401f8a1f8714a4ee037e780d0a
Mr.Chi <chiqingjun@gmail.com>
2021-07-04 14:49:04 +0800

    Update router for query params

--- tutorial/api/query_params.py
+++ tutorial/api/query_params.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 
 router = APIRouter()
@@ -24,3 +24,52 @@ def search_products(category: str, keyword: Optional[str] = None):
 )
 def search_shawshank(innocent: bool = False):
     return {"innocent": innocent}
+
+
+@router.get("/concubines/", summary="Query parameters with additional validation")
+def search_concubines(
+    hh: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=2,
+        regex=r"^(0?[1-9]|1[0-2])$",
+    ),
+    hhmm: str = Query(
+        ..., min_length=4, max_length=5, regex=r"^(0?[1-9]|1[0-2]):[0-5][0-9]$"
+    ),
+):
+    return {"hh": hh, "hhmm": hhmm}
+
+
+@router.get("/titanic/", summary="Query parameter list / multiple value")
+def show_titanic(
+    actors: Optional[list[str]] = Query(None),
+    roles: list[str] = Query(["Jack", "Rose"]),
+):
+    return {"actors": actors, "roles": roles}
+
+
+@router.get("/schindler/", summary="Query parameter with metadata")
+def get_schindler_list(
+    name: Optional[str] = Query(
+        None,
+        title="name of Jew",
+        description="The name of Jew which is choosen by Schindler",
+        min_length=3,
+    )
+):
+    return {"name": name}
+
+
+@router.get("/inception/", summary="Alias query parameter")
+def show_inception(
+    ring: str = Query("darry ring", alias="wedding-ring"),
+):
+    return {"ring": ring}
+
+
+@router.get("/hachi/", summary="Deprecated query parameter")
+def get_tale_of_hachi(
+    professor: Optional[str] = Query(None, deprecated=True),
+):
+    return {"professor": professor}
